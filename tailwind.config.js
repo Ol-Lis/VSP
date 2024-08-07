92021a382c742abd3bf06c57bad13be8e430254d
Dmytro Shynkarenko <dmytroshynkarenko26@gmail.com>
2024-08-07 20:22:08 +0300

    fix style.scss. Delete old style

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,15 +5,6 @@ module.exports = {
     fontFamily: {
       'body': ['Montserrat'],
     },
-    fontSize: {
-      sm: ['16px', '24px'],
-      base: ['16px', '24px'],
-      xl: '1.25rem',
-      '2xl': '1.563rem',
-      '3xl': '1.953rem',
-      '4xl': '2.441rem',
-      '5xl': '3.052rem',
-    },
     extend: {},
   },
   plugins: [],
